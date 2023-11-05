3adc8d0f64cbda20d7bf7002e14609c663d3bdaf
pilicorredor <pilicorredor111@gmail.com>
2023-11-05 16:22:01 -0500

    update color functions

--- src/pages/algorithm-RR/AlgorithmRR.jsx
+++ src/pages/algorithm-RR/AlgorithmRR.jsx
@@ -2,18 +2,111 @@ import React, { useMemo, useState } from "react";
 import { Queue } from "../../utilities/Queue";
 import ProcessInput from "../../components/process-input/ProcessInput";
 
-const quantum = 3;
+const quantum = 4;
 let global_time = 0;
 let queue = new Queue();
 let n = 0;
 
 const processes = [
-  { process_name: "Proceso P1", arrival_time: 0, execution_time: 7 },
-  { process_name: "Proceso P2", arrival_time: 1, execution_time: 3 },
-  { process_name: "Proceso P3", arrival_time: 2, execution_time: 4 },
-  { process_name: "Proceso P4", arrival_time: 4, execution_time: 2 },
-  { process_name: "Proceso P5", arrival_time: 5, execution_time: 4 },
+  {
+    process_name: "Proceso P1",
+    arrival_time: 0,
+    execution_time: 7,
+    start_time: null,
+    finish_time: null,
+    return_time: null,
+    waiting_time: null,
+  },
+  {
+    process_name: "Proceso P2",
+    arrival_time: 1,
+    execution_time: 3,
+    start_time: null,
+    finish_time: null,
+    return_time: null,
+    waiting_time: null,
+  },
+  {
+    process_name: "Proceso P3",
+    arrival_time: 2,
+    execution_time: 4,
+    start_time: null,
+    finish_time: null,
+    return_time: null,
+    waiting_time: null,
+  },
+  {
+    process_name: "Proceso P4",
+    arrival_time: 4,
+    execution_time: 2,
+    start_time: null,
+    finish_time: null,
+    return_time: null,
+    waiting_time: null,
+  },
+  {
+    process_name: "Proceso P5",
+    arrival_time: 5,
+    execution_time: 4,
+    start_time: null,
+    finish_time: null,
+    return_time: null,
+    waiting_time: null,
+  },
 ];
+
+const processes2 = [
+  {
+    process_name: "Proceso P1",
+    arrival_time: 0,
+    execution_time: 7,
+    start_time: null,
+    finish_time: null,
+    return_time: null,
+    waiting_time: null,
+  },
+  {
+    process_name: "Proceso P2",
+    arrival_time: 1,
+    execution_time: 3,
+    start_time: null,
+    finish_time: null,
+    return_time: null,
+    waiting_time: null,
+  },
+  {
+    process_name: "Proceso P3",
+    arrival_time: 2,
+    execution_time: 4,
+    start_time: null,
+    finish_time: null,
+    return_time: null,
+    waiting_time: null,
+  },
+  {
+    process_name: "Proceso P4",
+    arrival_time: 4,
+    execution_time: 2,
+    start_time: null,
+    finish_time: null,
+    return_time: null,
+    waiting_time: null,
+  },
+  {
+    process_name: "Proceso P5",
+    arrival_time: 5,
+    execution_time: 4,
+    start_time: null,
+    finish_time: null,
+    return_time: null,
+    waiting_time: null,
+  },
+];
+
+const result_colors = processes.map((process) => ({
+  process_name: process.process_name,
+  colors: [],
+}));
 
 const RoundRobin = ({
   formFields,
@@ -21,14 +114,24 @@ const RoundRobin = ({
   handleProcess,
   processList,
 }) => {
-  const [process_details, set_process_details] = useState(processes);
+  console.log("perdio por loca", processList);
+  
+
+  const [process_details, set_process_details] = useState([]);
 
   useMemo(() => {
-    const copie = process_details;
-    copie.map((item) => (item.number = 1));
-    set_process_details(copie);
-    console.log("copie-----", copie);
-  }, [process_details]);
+    const copy_list = processList.map(item => ({
+      ...item,
+      start_time: null,
+      finish_time: null,
+      return_time: null,
+      waiting_time: null
+    }));
+  
+    set_process_details(copy_list);
+  }, [processList]);
+
+  console.log('----',process_details)
 
   //Funcion que ordena los procesos segun el tiempo de llegada
   const order_processes = () => {
@@ -64,7 +167,103 @@ const RoundRobin = ({
       global_time++;
     }
   };
-
+  //funcion para actualizar los detalles de cada proceso, como el tiempo final, tiempo de espera,
+  //tiempo de retorno...
+  const update_details = (current_process, global_time) => {
+    current_process.finish_time =
+      global_time + current_process.execution_time - 1;
+    current_process.return_time =
+      current_process.finish_time - current_process.arrival_time + 1;
+    if (current_process.start_time === null) {
+      current_process.start_time = global_time;
+    }
+    for (let index = 0; index < processes2.length; index++) {
+      if (processes2[index].process_name === current_process.process_name) {
+        current_process.waiting_time =
+          current_process.return_time - processes2[index].execution_time;
+      }
+    }
+    console.log(processes);
+  };
+  //Funcion para actualizar el color verde (ejecución) de los procesos en la tabla
+  const update_color_green = (current_process, index, green) => {
+    if (current_process.execution_time <= quantum) {
+      for (let i = 0; i < current_process.execution_time; i++) {
+        result_colors[index].colors.push(green);
+      }
+    } else if (current_process.execution_time > quantum) {
+      for (let i = 0; i < quantum; i++) {
+        result_colors[index].colors.push(green);
+      }
+    }
+  };
+  //Funcion para actualizar el color blanco (no han entrado) de los procesos en la tabla
+  const update_color_white = () => {
+    let white = "blanco";
+    for (let index = 0; index < result_colors.length; index++) {
+      for (let j = 0; j < processes.length; j++) {
+        if (result_colors[index].process_name === processes[j].process_name) {
+          for (let k = processes[j].arrival_time; k > 0; k--) {
+            result_colors[index].colors[k - 1] = white;
+          }
+        }
+      }
+    }
+  };
+  //Funcion para actualizar el color amarillo (espera) de los procesos en la tabla
+  const update_color_yellow = (current_process, index, yellow) => {
+    if (current_process.execution_time <= quantum) {
+      for (let i = 0; i < current_process.execution_time; i++) {
+        result_colors[index].colors.push(yellow);
+      }
+    } else if (current_process.execution_time > quantum) {
+      for (let i = 0; i < quantum; i++) {
+        result_colors[index].colors.push(yellow);
+      }
+    }
+  };
+  //Funcion para actualizar el color rojo (terminado) de los procesos en la tabla
+  const update_color_last_green = (process, green) => {
+    let colors = process.colors;
+    let last_green = -1;
+    for (let i = process.colors.length; i >= 0; i--) {
+      if (colors[i] === green) {
+        last_green = i;
+        break;
+      }
+    }
+    if (last_green !== -1) {
+      for (let i = process.colors.length - 1; i >= 0; i--) {
+        if (colors[i] === green) {
+          break;
+        } else {
+          colors.pop();
+        }
+      }
+    }
+  };
+  //Funcion para actualizar todos los colores de los procesos en la tabla
+  const update_colors = (current_process) => {
+    let green = "#verde";
+    let yellow = "amarillo";
+    for (let index = 0; index < result_colors.length; index++) {
+      if (result_colors[index].process_name === current_process.process_name) {
+        update_color_green(current_process, index, green);
+      } else {
+        update_color_yellow(current_process, index, yellow);
+      }
+    }
+    for (let index = 0; index < processes.length; index++) {
+      if (processes[index].execution_time === 0) {
+        for (let i = 0; i < result_colors.length; i++) {
+          if (result_colors[i].process_name === processes[index].process_name) {
+            update_color_last_green(result_colors[i], green);
+          }
+        }
+      }
+    }
+  };
+  //Función principal
   const round_robin = () => {
     let current_process;
     while (!queue.isEmpty()) {
@@ -72,13 +271,12 @@ const RoundRobin = ({
       if (current_process.execution_time === 0) {
         current_process = queue.dequeue();
       } else {
-        console.log("tiempo global: ", global_time);
-        console.log(current_process);
+        update_colors(current_process);
+        update_details(current_process, global_time);
+        review_process_list(current_process);
         if (current_process.execution_time <= quantum) {
-          review_process_list(current_process);
           current_process.execution_time = 0;
         } else if (current_process.execution_time > quantum) {
-          review_process_list(current_process);
           current_process.execution_time -= quantum;
           if (current_process.execution_time !== 0) {
             queue.enqueue(current_process);
@@ -86,9 +284,10 @@ const RoundRobin = ({
         }
       }
     }
+    update_color_white();
+    console.log("colors ", result_colors);
   };
 
-  console.log("buenasssss", process_details);
   order_processes();
   add_queue_begin();
   round_robin();
