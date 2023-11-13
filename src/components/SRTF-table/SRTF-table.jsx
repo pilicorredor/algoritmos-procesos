38eb86712ea9a33f32bd4935a40affbd4f56873c
martin <martin.corredor@rappi.com>
2023-11-12 21:30:20 -0500

    Fix styles

--- src/components/SRTF-table/SRTF-table.jsx
+++ src/components/SRTF-table/SRTF-table.jsx
@@ -3,8 +3,7 @@ import './styles.css';
 
 const SRTFTable = ({ queue }) => {
   return (
-    <div className='SRTF-Table'>
-      <h3>Orden de ejecucion:</h3>
+    <div>
         <div className="box-container">
                 {queue.map((item, index) => (             
                     <div className="box" key={index}>{item}</div>
